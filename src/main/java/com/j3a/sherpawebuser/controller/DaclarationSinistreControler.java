9a0f615fb1c41a75698e52d9d1149b93f7228dae
PCALekerand <kerandelniangoran@gmail.com>
2015-04-30 17:43:22 +0000

    controlerDeclaration

---
+++ src/main/java/com/j3a/sherpawebuser/controller/DaclarationSinistreControler.java
@@ -0,0 +1,12 @@
+package com.j3a.sherpawebuser.controller;
+
+import javax.inject.Singleton;
+
+@Singleton
+public class DaclarationSinistreControler {
+
+	public DaclarationSinistreControler() {
+		// TODO Auto-generated constructor stub
+	}
+
+}
